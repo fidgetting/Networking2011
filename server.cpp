3f4ead10984b32f7ffcff0ff049b20cc732de139
Jef Mallal <eclipse5769@gmail.com>
2011-09-01 10:55:44 -0600

    updates to server

--- server.cpp
+++ server.cpp
@@ -6,3 +6,40 @@
  */
 
 //TODO just testing git adding and stuff
+
+#include <getopt.h>
+
+#define BACKLOG 10
+
+//TODO
+void usage(const string& exe) {
+	exit(-1);
+}
+
+int main(int argc, char** argv) {
+  bool tcp = false;
+	bool t, p;
+    t = p = false;
+
+  while((c = getopt(argc, argv, "t:p:")) != 0) {
+      switch(c) {
+        case 't':
+          t = true;
+          if(strcmp(optarg, "udp") == 0)
+            break;
+          if(strcmp(optarg, "tcp") == 0) {
+            tcp = true;
+            break;
+          }
+          usage(argv[0]);
+          break;
+       case 'p':
+          port = optarg;
+          p = true;
+          break;
+       default:
+          usage(argv[0]);
+          break;
+      }
+  }
+}
