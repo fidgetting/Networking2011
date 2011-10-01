d7ebe2a54a844b4f9d8dd35ddee51e9a33f5608d
Alex Norton <fidgetting@gmail.com>
2011-09-30 18:45:42 -0600

    adding the interface file, awget.c and ss.c can now be developed seperately

---
+++ proj2/awget.h
@@ -0,0 +1,36 @@
+/*
+ * awget.h
+ *
+ *  Created on: Sep 30, 2011
+ *      Author: Alex Norton,
+ *              Nick Savage,
+ *              Joshua Sorensen,
+ *              Jef Mallal
+ */
+
+#ifndef AWGET_H_INCLUDE
+#define AWGET_H_INCLUDE
+
+typedef unsigned char uint8_t;
+typedef unsigned long uint32_t;
+
+#pragma pack(1)
+typedef struct {
+	uint8_t  version;
+	uint32_t l_size;
+} list_header;
+
+#pragma pack(1)
+typedef struct {
+	uint8_t version;
+	char host[256];
+	char port[8];
+} list_element;
+
+#pragma pack(1)
+typedef struct {
+	uint8_t  version;
+	uint32_t f_size;
+} file_header;
+
+#endif /* AWGET_H_INCLUDE */
